cbce3a6f18401cf9ac47efc4451d778d8be85780
alakakaka <alakakaka@yandex.com>
2019-03-18 16:28:07 +0800

    mechart_ios init

---
+++ mechart/mechart.h
@@ -0,0 +1,17 @@
+/**
+ *  @file
+ *  @copyright defined in mechart_ios/LICENSE
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for echart.
+FOUNDATION_EXPORT double mechartVersionNumber;
+
+//! Project version string for echart.
+FOUNDATION_EXPORT const unsigned char mechartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <echart/PublicHeader.h>
+
+
+#import <mechart/CLib2.h>
